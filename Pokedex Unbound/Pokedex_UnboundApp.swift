09a20088a92f05a9dcd29f3f8822c61b76218949
‘NassimRihane’ <nassim.rihane@centrale-med.fr>
2025-10-27 13:38:55 +0900

    Initial Commit

---
+++ Pokedex Unbound/Pokedex_UnboundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_UnboundApp.swift
+//  Pokedex Unbound
+//
+//  Created by Nassim Rihane on 27/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_UnboundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
